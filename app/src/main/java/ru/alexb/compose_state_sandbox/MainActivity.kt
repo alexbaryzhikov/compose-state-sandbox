0b287259fd653063d2929de65221091a9516d880
alexb <aleksiarts@gmail.com>
2021-11-02 00:57:16 +0300

    Add CounterButton example

--- app/src/main/java/ru/alexb/compose_state_sandbox/MainActivity.kt
+++ app/src/main/java/ru/alexb/compose_state_sandbox/MainActivity.kt
@@ -1,11 +1,32 @@
 package ru.alexb.compose_state_sandbox
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = CounterButton(findViewById(R.id.button), Counter())
+        button.initialize()
+    }
+}
+
+class Counter {
+    var value: Int = 0
+
+    fun increment() {
+        value++
+    }
+}
+
+class CounterButton(private val button: Button, private val counter: Counter) {
+    fun initialize() {
+        button.text = "Counter: ${counter.value}"
+        button.setOnClickListener {
+            counter.increment()
+        }
     }
 }
